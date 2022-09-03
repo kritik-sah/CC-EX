ddf03c0618033782c6965843cc38c6a816b46f22
Kritik <kritiksah0@gmail.com>
2022-09-03 17:47:16 +0530

    walletconnect

--- packages/carboncredit-ui/components/Layout.tsx
+++ packages/carboncredit-ui/components/Layout.tsx
@@ -1,14 +1,57 @@
-import { Box, Button, HStack, Heading, Text, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Text,
+  Link,
+  useDisclosure,
+  VStack,
+  StackDivider,
+  useToast,
+  Portal,
+  Avatar,
+  Flex,
+  Input,
+  useClipboard,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
+
+import { FaCopy, FaRegCopy } from "react-icons/fa";
 import React, { useState, useEffect } from "react";
-import { useAccount, useConnect } from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
+
 
 const Layout = function ({ children }: { children: React.ReactNode }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
   const { address, isConnected, connector } = useAccount();
+
+  const { hasCopied, onCopy } = useClipboard(address);
   const [isSSR, setIsSSR] = useState(true);
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
-  });
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+  const { disconnect } = useDisconnect();
+
   useEffect(() => {
     setIsSSR(false);
   }, []);
@@ -23,23 +66,99 @@ const Layout = function ({ children }: { children: React.ReactNode }) {
       >
         <Heading color={"white"}>Carbon Credit</Heading>
         {!isSSR && isConnected ? (
-          <Text
-            borderRadius={"2xl"}
-            bgColor={"cyan.900"}
-            px={"3"}
-            color={"white"}
-          >
-            {address?.toString().slice(0, -36)}...
-            {address?.toString().substring(38)}
-          </Text>
+          <>
+            <Menu>
+              <MenuButton>
+                <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+              </MenuButton>
+              <Portal>
+                <MenuList>
+                  <MenuItem>
+                    <Flex
+                      mb={2}
+                      align="center"
+                      justify="space-between"
+                      w="full"
+                    >
+                      <Text>
+                        {address?.toString().slice(0, -36)}...
+                        {address?.toString().substring(38)}
+                      </Text>
+                      <Button onClick={onCopy} ml={2}>
+                        {hasCopied ? <FaCopy /> : <FaRegCopy />}
+                      </Button>
+                    </Flex>
+                  </MenuItem>
+
+                  <MenuItem onClick={() => disconnect()}>Logout</MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </>
         ) : (
-          <Button
-            onClick={() => {
-              connect();
-            }}
-          >
-            Connect Metamask
-          </Button>
+          <>
+            
+            <Button
+              onClick={onOpen}
+              
+            >
+              Connect
+            </Button>
+            <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Login</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Text fontWeight="bold" mb="1rem">
+                    Connect your wallet
+                  </Text>
+                  <div>
+                    {connectors.map((connector) => (
+                      <>
+                        <VStack
+                          divider={<StackDivider borderColor="gray.200" />}
+                          spacing={4}
+                          align="stretch"
+                        >
+                          <Button
+                            disabled={!connector.ready}
+                            key={connector.id}
+                            onClick={() => connect({ connector })}
+                            colorScheme="teal"
+                            variant="ghost"
+                          >
+                            {connector.name}
+                            {isLoading &&
+                              connector.id === pendingConnector?.id &&
+                              " (connecting)"}
+                          </Button>
+                          
+                          
+                        </VStack>
+                        {/* {error && (
+                              <div>
+                              {toast({
+                                title: error.message,
+                                status: "error",
+                                isClosable: true,
+                              })}
+                              </div>
+                            
+                          )} */}
+                      </>
+                    ))}
+                  </div>
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
         )}
       </HStack>
       {children}
